8c303a7d22f1a1343539a4873731784c8c6b4771
Grace-aloo <graceaaloo@gmail.com>
2022-11-23 15:07:30 +0300

    completed assignement

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(value){
   // Write your code here!
+  if (value <= 400){
+    return "This one is on me!";
+  }
+  else if (value > 2000 && value < 2500){
+    return "I will gladly take your thirty bucks."
+  }
+  else if ( value > 2500 ){
+    return "No can do."
+  }
+  
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(destination){
   // Write your code here!
+  
+  return  destination ===  "NYC" ? "Ok, sounds good." : "No go.";
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  const  tip = 'generous';
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case "generous":
+       return "Thank you so much.";
+       break;
+    case 'not as generous':
+       return "Thank you.";
+       break;
+    default :
+       return "Bye.";
+  }
 }
